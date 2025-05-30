135441aae880180190a0dbcfd4de3a5ebfc7e84d
mr-ssc <soyabchhuvara3@gmail.com>
2025-05-30 14:45:38 +0530

    first commit

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -110,7 +110,7 @@ export default function Contact() {
                         <div className="CONTACT-map-container">
                             <iframe
                                 title="Google Maps Location"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.1574130135937!2d73.29055257510558!3d23.45478577887324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395df1773d6b4c6b%3A0x37475ef462ce6dd5!2sJIlani%20steel%20and%20cement!5e0!3m2!1sen!2sin!4v1748409962544!5m2!1sen!2sin"
+                                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3660.191293043431!2d73.28908327532679!3d23.453563378874104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDI3JzEyLjgiTiA3M8KwMTcnMzAuMCJF!5e0!3m2!1sen!2sin!4v1748596366242!5m2!1sen!2sin"
                                 width="100%"
                                 height="300"
                                 style={{ border: 0 }}
